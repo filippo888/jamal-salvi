bce9f2230e40e5ccd3c564335ad888a5343cd3b6
Kepler0ne <jamal.salvi@gmail.com>
2022-08-24 23:11:20 +0200

    Studies component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -66,7 +66,7 @@ const AboutMe = () => {
                     leftImage: false,
                     withImage: true,
                     text: {  
-                        caption: "Sexxy ",
+                        caption: "Studies ",
                         title: " Undergraduate Studies",
                         body: "After finishing High School I had already planned to enroll in an University abroad. I didn't have specifics but I knew that I wanted two things: 1. To go abroad and 2. To pick something technical. \n Point 1. comes from my everlasting thrive to be independent and my passion for new and challenging experiences, point 2. comes from the fact that I didn't exactly excell in math and physics in High School and hence wanted to fill that hole. \n The two things brought me to enroll in a Bachelor of Science in Mechatronics at SDU, in Denmark. Filled with excitement and at the same time fear, I left my family and friends and went to Denmark with my motorcycle. \n The program indeed helped me to make up for my previous deficency in math and physics and my ability as a student granted me some wonderful academic accomplishments. I also landed a part-time job to pay my rent and further develop my technical skills (see resume section). \n During the study, I have been inexpectedly fascinated by a mandtory class that is not as technical as everything else I have studies: Project Management. Following the course and completing assignments made me grow an interest in management as a career. \n As for my Bachelor thesis, I decided to once again go for a blockchain-related research. I collaborated with a small Danish company to perform a \"feasibility study on the tokenization of Carbon Offsetting Credits \" .",
                     },
